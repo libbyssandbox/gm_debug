b28c38a015a6424a845024aed319e9fb52eb69d3
Zach <147861361+1054009@users.noreply.github.com>
2024-05-19 05:43:48 -0400

    Include Lua Shared

--- gm_debug/premake5.lua
+++ gm_debug/premake5.lua
@@ -4,6 +4,7 @@ include("./garrysmod_common/")
 CreateWorkspace({name = "debug", abi_compatible = false, path = "projects/"})
 	CreateProject({serverside = true, source_path = "src", manual_files = false})
 		IncludeHelpersExtended()
+		IncludeLuaShared()
 		IncludeSDKCommon()
 		IncludeSDKTier0()
 		files({
